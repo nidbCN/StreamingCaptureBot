2e830269e1da10f8555625e58d23b59201e356dc
Gaein nidb <mail@gaein.cn>
2024-11-15 18:02:40 +0800

    opt: optimize base image

--- build-base.sh
+++ build-base.sh
@@ -5,10 +5,10 @@ if [ ! -f "/tmp/bot_build_temp/ffmpeg-n7.0.2-6-g7e69129d2f-linux64-gpl-shared-7.
     wget -O /tmp/bot_build_temp/ffmpeg-n7.0.2-6-g7e69129d2f-linux64-gpl-shared-7.0.tar.xz "https://github.com/BtbN/FFmpeg-Builds/releases/download/autobuild-2024-08-31-12-50/ffmpeg-n7.0.2-6-g7e69129d2f-linux64-gpl-shared-7.0.tar.xz"
 fi
 
-xz -d -k /tmp/bot_build_temp/ffmpeg-n7.0.2-6-g7e69129d2f-linux64-gpl-shared-7.0.tar.xz # /tmp/ffmpeg-n7.0.2-6-g7e69129d2f-linux64-gpl-shared-7.0.tar
+mkdir -p ffmpeg
 
-mv /tmp/bot_build_temp/ffmpeg-n7.0.2-6-g7e69129d2f-linux64-gpl-shared-7.0.tar ffmpeg.tar
+tar --strip-components=1 -xf /tmp/bot_build_temp/ffmpeg-n7.0.2-6-g7e69129d2f-linux64-gpl-shared-7.0.tar.xz -C ffmpeg
 
 sudo docker build . -f Dockerfile.base -t registry.cn-beijing.aliyuncs.com/nidb-cr/camera-capture-bot-base:7.0.2-6
 
-rm ffmpeg.tar
+rm -r ffmpeg
