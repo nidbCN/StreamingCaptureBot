a34d9da7b9b6a0addb801cb3e85cd9ddfaee5eb3
Gaein_nidb <mail@gaein.cn>
2024-12-17 10:37:09 +0800

    fix: ffmpeg timespan overflow check, use API to calc timespan

--- StreamingCaptureBot.Core/FfMpeg.Net/Utils/TimeSpanUtil.cs
+++ StreamingCaptureBot.Core/FfMpeg.Net/Utils/TimeSpanUtil.cs
@@ -4,20 +4,24 @@ namespace StreamingCaptureBot.Core.FfMpeg.Net.Utils;
 
 public static class TimeSpanUtil
 {
+    private static AVRational _ffMpegTimeBaseRational = new()
+    {
+        num = 1,
+        den = ffmpeg.AV_TIME_BASE
+    };
+
     public static TimeSpan? FromFfmpeg(long value, AVRational timebase)
     {
         if (timebase.den <= 0 || timebase.num < 0 || value == ffmpeg.AV_NOPTS_VALUE)
             return null;
 
-        var microSec = (long)(
-            (Int128)(value * timebase.num) * ffmpeg.AV_TIME_BASE
-            / timebase.den);
+        var microSec = ffmpeg.av_rescale_q(value, timebase, _ffMpegTimeBaseRational);
 
-        if (microSec > TimeSpan.MaxValue.TotalSeconds)
+        if (microSec > TimeSpan.MaxValue.TotalMicroseconds)
             return TimeSpan.MaxValue;
 
         // ReSharper disable once ConvertIfStatementToReturnStatement
-        if (microSec < TimeSpan.MinValue.TotalSeconds)
+        if (microSec < TimeSpan.MinValue.TotalMicroseconds)
             return TimeSpan.MinValue;
 
         return TimeSpan.FromMicroseconds(microSec);
