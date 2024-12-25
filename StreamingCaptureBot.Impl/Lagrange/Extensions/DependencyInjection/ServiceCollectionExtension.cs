d4eeb218e6001c2ccb13f1056819c12d5850aa05
Gaein nidb <mail@gaein.cn>
2024-12-25 10:15:35 +0800

    fix: lagrange impl configure

--- StreamingCaptureBot.Impl/Lagrange/Extensions/DependencyInjection/ServiceCollectionExtension.cs
+++ StreamingCaptureBot.Impl/Lagrange/Extensions/DependencyInjection/ServiceCollectionExtension.cs
@@ -1,6 +1,7 @@
 ﻿using Lagrange.Core.Common.Interface;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using StreamingCaptureBot.Impl.Lagrange.Options;
 using StreamingCaptureBot.Impl.Lagrange.Services;
 
@@ -8,21 +9,29 @@ namespace StreamingCaptureBot.Impl.Lagrange.Extensions.DependencyInjection;
 
 public static class ServiceCollectionExtension
 {
-    public static IServiceCollection AddLagrangeBots(this IServiceCollection s)
+    public static IServiceCollection AddLagrangeBots(this IServiceCollection s, Action<LagrangeImplOption>? config = null)
         => s.AddSingleton<StoreService>()
             .AddSingleton(provider =>
             {
-                var configSection = provider.GetRequiredService<IConfiguration>()
-                    .GetSection(nameof(LagrangeImplOption));
-                s.Configure<LagrangeImplOption>(configSection);
+                // use configured options
+                var implOption = provider.GetService<IOptions<LagrangeImplOption>>()?.Value;
+                
+                // use config file
+                if (implOption is null)
+                {
+                    var configSection = provider.GetRequiredService<IConfiguration>()
+                        .GetSection(nameof(LagrangeImplOption));
+                    implOption = configSection.Get<LagrangeImplOption>() ?? new();
+                }
 
-                var config = configSection.Get<LagrangeImplOption>();
+                // use user-defined config
+                config?.Invoke(implOption);
 
                 var storeService = provider.GetRequiredService<StoreService>();
                 var deviceInfo = storeService.ReadDeviceInfo();
                 var keyStore = storeService.ReadKeyStore();
 
-                return BotFactory.Create(config?.LagrangeConfig ?? new(), deviceInfo, keyStore);
+                return BotFactory.Create(implOption.LagrangeConfig, deviceInfo, keyStore);
             })
             .AddHostedService<LagrangeHost>();
 }
